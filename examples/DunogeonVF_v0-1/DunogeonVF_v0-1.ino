465c578f0ef57b754b56a28691758cc79d43a849
julien leresteux <leresteuxj@gmail.com>
2022-12-13 12:07:15 +0100

    Update and rename examples/DunoeonVF_v0-1/DunoeonVF_v0-1.ino to examples/DunogeonVF_v0-1/DunogeonVF_v0-1.ino

---
+++ examples/DunogeonVF_v0-1/DunogeonVF_v0-1.ino
@@ -0,0 +1,17 @@
+/*
+Ceci est la V0.1, uniquement en français.
+Lancez le programme et le moniteur série.
+Bonne aventure.
+*/
+
+#include "dunogeon.h"
+
+DUNOGEON mondonjon;
+
+void setup() {
+mondonjon.begin();
+}
+
+void loop() {
+}
+
